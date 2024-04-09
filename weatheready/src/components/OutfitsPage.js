4aa6d81432b4881942da0c5f2c5d6dd3de2c9e20
Diya Agrawal <130734531+diyaag2004@users.noreply.github.com>
2024-04-09 15:02:10 +0530

    images in Outfit suggestion

--- weatheready/src/components/OutfitsPage.js
+++ weatheready/src/components/OutfitsPage.js
@@ -3,6 +3,30 @@ import { useContext } from 'react';
 import { WeatherContext } from '../contexts/WeatherContext';
 import { Link, useNavigate } from 'react-router-dom';
 import '../App.css';
+import jeans1 from '..//images/jeans/jeans1.png';
+import jeans2 from '../images/jeans/jeans2.png';
+import jeans3 from '../images/jeans/jeans3.png';
+import jeans4 from '../images/jeans/jeans4.png';
+import jeans5 from '../images/jeans/jeans5.png';
+import jeans6 from '../images/jeans/jeans6.png';
+import jeans7 from '../images/jeans/jeans7.png';
+import jeans8 from '../images/jeans/jeans8.png';
+import shirt1 from '../images/shirt/shirt1.png';
+import shirt2 from '../images/shirt/shirt2.png';
+import shirt3 from '../images/shirt/shirt3.png';
+import shirt4 from '../images/shirt/shirt4.png';
+import shirt5 from '../images/shirt/shirt5.png';
+import shirt6 from '../images/shirt/shirt6.png';
+import shirt7 from '../images/shirt/shirt7.png';
+import shirt8 from '../images/shirt/shirt8.png';
+import shirt9 from '../images/shirt/shirt9.png';
+import shirt10 from '../images/shirt/shirt10.png';
+import shirt11 from '../images/shirt/shirt11.png';
+
+import shirt13 from '../images/shirt/shirt13.png';
+import shirt14 from '../images/shirt/shirt14.png';
+
+
 const OutfitsPage = () => {
   const { weatherData1, setWeatherData1 } = useContext(WeatherContext);
   const navigate = useNavigate();
@@ -15,7 +39,7 @@ const OutfitsPage = () => {
   const temperature = weatherData1.main.temp;
   const humidity = weatherData1.main.humidity;
   const windSpeed = weatherData1.wind.speed;
-
+ 
   const fetchDressRecommendation = async () => {
     setLoading(true);
     setError(false);
@@ -59,16 +83,32 @@ const OutfitsPage = () => {
         <div>
           <h4>City: {weatherData1.name}</h4>
           <h6>Temperature: {weatherData1.main.temp}°C</h6>
-          <h6>Humidity: {weatherData1.weather[0].description}%</h6>
+          <h6>Weather: {weatherData1.weather[0].description}%</h6>
+          <h6>Humidity: {weatherData1.main.humidity}%</h6>
           <h6>Wind Speed: {weatherData1.wind.speed} Km/s</h6>
         </div>
       )}
 
       {loading ? <p className='loading'>Telling you dress Recommendation...</p> : null}
+     <div class="image-container">
       <div>
-        {shirtPrediction && <h2>Predicted Shirt: {shirtPrediction}</h2>}
-        {jeansPrediction && <h2>Predicted Jeans: {jeansPrediction}</h2>}
+        {shirtPrediction && (
+          <div className="image-container">
+            {/* <h2>Predicted Shirt: {shirtPrediction}</h2> */}
+            <img src={require(`../images/shirt/${shirtPrediction}.png`)} alt={shirtPrediction} />
+        
+            
+          </div>
+        )}
+        {jeansPrediction && (
+          <div className="image-container">
+            {/* <h2>Predicted Jeans: {jeansPrediction}</h2> */}
+            <img src={require(`../images/jeans/${jeansPrediction}.png`)} alt={jeansPrediction} />
+          </div>
+        )}
       </div>
+      </div>
+     
 
       <button onClick={handleDressRecommendation} className='dressp'>Get Outfits Recommendations</button>
 
---
+++ weatheready/src/components/dressimport.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function dressimport() {
+  return (
+    <div></div>
+  )
+}
+export default dressimport
+//export {jeans1,jeans2,jeans3,jeans4,jeans5,jeans6,jeans7,jeans8,shirt1,shirt2,shirt3,shirt4,shirt5,shirt6,shirt7,shirt8,shirt9,shirt10,shirt11,shirt13,shirt14}
