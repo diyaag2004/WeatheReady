7d975ab5a8b52db514d99604cfda9348e51081b4
Mitali Dixit <mitali.dixit04@gmail.com>
2024-04-09 12:44:50 +0530

    changes

--- weatheready/src/App.js
+++ weatheready/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'; 
-import Header from './components/Header';
-import GlassMorphismCards from './components/GlassMorphismCards';
 import Homepage from './components/Homepage';
 
 function App() {
