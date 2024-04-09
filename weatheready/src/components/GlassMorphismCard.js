f4e61a9f6516abd8e5355cd0613d464ea095fdca
Mitali Dixit <mitali.dixit04@gmail.com>
2024-04-09 12:31:03 +0530

    create 3 glass morphism cards

---
+++ weatheready/src/components/GlassMorphismCard.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './GlassMorphismCards.css';
+import VanillaTilt from 'vanilla-tilt';
+
+
+const GlassMorphismCards = () => {
+  useEffect(() => {
+    VanillaTilt.init(document.querySelectorAll(".card"), {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 1,
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="card" id="card1">
+        <div className="content">
+          <h2>01</h2>
+          <h3>Card One</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum tempore ratione odit libero facere unde temporibus vel consequuntur quae ad, distinctio tenetur dicta dolorem aliquam, voluptatibus repudiandae, fuga ducimus corrupti.</p>
+          <Link to="/outfits">Read More</Link>
+        </div>
+      </div>
+      <div className="card" id="card2">
+        <div className="content" >
+          <h2>02</h2>
+          <h3>Card Two</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum tempore ratione odit libero facere unde temporibus vel consequuntur quae ad, distinctio tenetur dicta dolorem aliquam, voluptatibus repudiandae, fuga ducimus corrupti.</p>
+          <Link to="/outfits">Read More</Link>
+        </div>
+      </div>
+      <div className="card" id="card3">
+        <div className="content">
+          <h2>03</h2>
+          <h3>Card Three</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum tempore ratione odit libero facere unde temporibus vel consequuntur quae ad, distinctio tenetur dicta dolorem aliquam, voluptatibus repudiandae, fuga ducimus corrupti.</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GlassMorphismCards;
