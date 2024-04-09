43bf7a8e4222ade3eb23a0482c30224f7dc2c565
Mitali Dixit <mitali.dixit04@gmail.com>
2024-04-09 12:27:27 +0530

    added Header section

---
+++ weatheready/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css'; 
+const Header = () => {
+  return (
+    <header className="container-fluid text-center" id="header">
+      <div className="row">
+        <div className="col header-content">
+        <img src="assets/images/WeatherIcon.png" height="160" width="200" alt="Logo image" className='cloud-img' />
+          <h1 id="header-title" className="text-uppercase">
+            WeatheReady
+          </h1>
+          <div className="line light" id="main-header-line"></div>
+          
+          <p className="header-paragraph">
+            WeatheReady is a web app that tells you which clothing items you should wear today.
+            <br />
+            <span className="text-uppercase">Enter your location below!</span>
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
