b68be464d04af5bff7aadb9023205f62bcf7dfbd
Mitali Dixit <mitali.dixit04@gmail.com>
2024-04-09 12:20:11 +0530

    added navbar

---
+++ weatheready/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'; 
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light fixed-top" id="navbar">
+      <a className="navbar-brand" href="index.html">
+        <img src="assets/images/WeatheReady.jpg" height="50" width="200" alt="Logo image" />
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
